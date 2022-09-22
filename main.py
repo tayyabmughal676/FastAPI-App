1d2535e51ce97c9020c366fb009b59d7ccf457c8
thor <tayyabmughal676@gmail.com>
2022-09-22 14:27:32 +0500

    File Uploading Test

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from datetime import date
+from enum import Enum
 from typing import Union
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+
 
 app = FastAPI()
 # User Model Class
@@ -13,6 +15,12 @@ class User(BaseModel):
     joined: date
 
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
@@ -21,3 +29,27 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {
+        "code": 1,
+        "message": "Successful",
+        "data": [
+            {
+            "filename": file.filename,
+        }
+        ]
+    }
